685f7c4dfcf4129f9e29139fc21bd05d07c2b146
Josh Kodroff <josh@pulumi.com>
2025-09-03 12:02:54 -0400

    Add resources.

--- component/index.ts
+++ component/index.ts
@@ -1,21 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as azureNative from "@pulumi/azure-native";
 
-// TODO: Rename this class to reflect the name of the component, e.g. "VirtualNetworkArgs"
-export interface MyComponentArgs {
+export interface MyResourceGroupArgs {
   // TODO: Add input properties in the following form:
   // inputProperty: pulumi.Input<T>
 }
 
-// TODO: Rename this class to something descriptive, e.g. "VirutalNetwork"
-export class MyComponent extends pulumi.ComponentResource {
-  constructor(name: string, args: MyComponentArgs, opts: pulumi.CustomResourceOptions) {
-    // TODO: Replace `pulumi-components` with the name of your package, e.g. `networking`
-    // TODO: Replace `MyComponent` in this string with the name of your component.
-    super("pulumi-components:index:MyComponent", name, args, opts);
+export class MyResourceGroup extends pulumi.ComponentResource {
+  public readonly myResourceGroup: azureNative.resources.ResourceGroup;
+
+  constructor(name: string, args: MyResourceGroupArgs, opts: pulumi.CustomResourceOptions) {
+    super("pulumi-components:index:MyResourceGroup", name, args, opts);
+
+    this.myResourceGroup = new azureNative.resources.ResourceGroup(`${name}-resource-group`, {
+
+    });
 
     this.registerOutputs({
-      // TODO: Add all outputs here in the following form:
-      // outputName: this.someResource.someProperty,
-    })
+      myResourceGroup: this.myResourceGroup,
+    });
   }
 }
